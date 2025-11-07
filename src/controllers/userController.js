f4ef7ec5bc6166cb372629f382efaf4d2ed12d74
your_name <pastushenkodmitry2003@gmail.com>
2025-11-07 21:22:54 +0200

    user routes & controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,12 @@
-import { User } from "../models/user.js";
-import createHttpError from "http-errors";
-import { saveFileToCloudinary } from "../utils/saveFileToCloudinary.js";
+import createHttpError from 'http-errors';
+
+import { User } from '../models/user.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
 export const updateUserAvatar = async (req, res, next) => {
-if (!req.file) {
+  if (!req.file) {
     return next(createHttpError(400, 'No file'));
-}
+  }
   const result = await saveFileToCloudinary(req.file.buffer);
   const user = await User.findOneAndUpdate(
     { _id: req.user._id },
@@ -15,3 +16,19 @@ if (!req.file) {
 
   res.status(200).json({ url: user.avatar });
 };
+
+export async function getCurrentUser(req, res, next) {
+  const user = await User.findById(req.user._id);
+  if (!user) return next(createHttpError(404, 'User not found'));
+
+  res.status(200).json(user);
+}
+
+export async function updateUser(req, res, next) {
+  const user = await User.findOneAndUpdate({ _id: req.user._id }, req.body, {
+    new: true,
+  });
+  if (!user) return next(createHttpError(404, 'User not found'));
+
+  res.status(200).json(user);
+}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,23 @@
 import { Router } from 'express';
-import { authenticate } from '../middleware/authenticate.js';
-import { updateUserAvatar } from '../controllers/userController.js';
-import { upload } from '../middleware/multer.js';
 
+import { authenticate } from '../middleware/authenticate.js';
+import {
+  getCurrentUser,
+  updateUser,
+  updateUserAvatar,
+} from '../controllers/userController.js';
+import { upload } from '../middleware/multer.js';
 
 const router = Router();
 
-router.patch('/users/me/avatar', authenticate, upload.single('avatar'), updateUserAvatar);
+router.patch(
+  '/users/me/avatar',
+  authenticate,
+  upload.single('avatar'),
+  updateUserAvatar,
+);
+
+router.get('/users/me', authenticate, getCurrentUser);
+router.patch('/users/me', authenticate, updateUser);
 
 export default router;
