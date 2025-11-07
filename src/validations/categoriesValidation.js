7c738a36bfc0b0b8149f5ce316f02ac50dbe6abb
Dmode <dmode90@gmail.com>
2025-11-07 19:52:07 +0200

    min categories refine

--- src/validations/categoriesValidation.js
+++ src/validations/categoriesValidation.js
@@ -7,9 +7,8 @@ export const getAllCategoriesSchema = {
       'number.min': 'Page must be at least 1',
       'number.integer': 'Page must be an integer',
     }),
-    perPage: Joi.number().integer().min(4).max(20).default(4).messages({
-      'number.min': 'PerPage must be at least 4',
-      'number.max': 'PerPage must be at most 20',
+    perPage: Joi.number().integer().min(1).default(4).messages({
+      'number.min': 'PerPage must be at least 1',
       'number.integer': 'PerPage must be an integer',
     }),
   }),
