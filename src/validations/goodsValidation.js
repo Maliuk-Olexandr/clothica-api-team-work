f659859bffb6419bc699c057aea8c17344677dcd
Oleksandr Maliuk <maliukolexandr@gmail.com>
2025-11-08 15:18:27 +0200

    feat: implement caching for filter parameters and update validation schema

--- src/validations/goodsValidation.js
+++ src/validations/goodsValidation.js
@@ -1,44 +1,37 @@
 import { Joi, Segments } from 'celebrate';
 import { isValidObjectId } from 'mongoose';
 
-const CATEGORIES = [
-  'all',
-  '690c9ce6a6276289c98fc006',
-  '690c9ce6a6276289c98fc007',
-  '690c9ce6a6276289c98fc008',
-  '690c9ce6a6276289c98fc009',
-  '690c9ce6a6276289c98fc00a',
-  '690c9ce6a6276289c98fc00b',
-  '690c9ce6a6276289c98fc00c',
-  '690c9ce6a6276289c98fc00d',
-];
-
+import { getFilterParams } from '../utils/getFilterParams.js';
 import { GENDER, SIZES } from '../constants/const.js';
-export const getAllGoodsSchema = {
-  [Segments.QUERY]: Joi.object({
-    page: Joi.number().integer().min(1).default(1),
-    perPage: Joi.number().integer().min(5).max(20).default(8),
-    category: Joi.string()
-      .valid(...CATEGORIES)
-      .default('all'),
-    size: Joi.string()
-      .valid(...SIZES)
-      .optional(),
-    minPrice: Joi.number().integer().min(0).max(5299).default(0),
-    maxPrice: Joi.number().integer().min(0).max(5299).default(5299),
-    gender: Joi.string()
-      .valid(...GENDER)
-      .default(GENDER[0]),
-    sortBy: Joi.string().valid('_id', 'price.value').default('_id'),
-    sortOrder: Joi.string().valid('asc', 'desc').default('asc'),
-  }),
+
+export const getAllGoodsSchema = async () => {
+  const {categoryIds, minPrice, maxPrice} = await getFilterParams();
+  return {
+    [Segments.QUERY]: Joi.object({
+      page: Joi.number().integer().min(1).default(1),
+      perPage: Joi.number().integer().min(5).max(20).default(8),
+      category: Joi.string()
+        .valid(...categoryIds)
+        .default('all'),
+      size: Joi.string()
+        .valid(...SIZES)
+        .optional(),
+      minPrice: Joi.number().integer().min(minPrice).max(maxPrice).default(minPrice),
+      maxPrice: Joi.number().integer().min(0).max(maxPrice).default(maxPrice),
+      gender: Joi.string()
+        .valid(...GENDER)
+        .default(GENDER[0]),
+      sortBy: Joi.string().valid('_id', 'price.value').default('_id'),
+      sortOrder: Joi.string().valid('asc', 'desc').default('asc'),
+    }),
+  };
 };
 
 const objectIdValidator = (value, helpers) => {
   return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
 };
 
-// Схема для перевірки параметра studentId
+// Схема для перевірки параметра goodId
 export const goodIdSchema = {
   [Segments.PARAMS]: Joi.object({
     goodId: Joi.string().custom(objectIdValidator).required(),
