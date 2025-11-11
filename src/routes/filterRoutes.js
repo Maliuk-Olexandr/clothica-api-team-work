d0d68a016043d45e0e98f8e2a1dc6333f1c38087
Oleksandr Maliuk <maliukolexandr@gmail.com>
2025-11-11 02:04:11 +0200

    feat: add Swagger documentation for filters endpoint

--- src/routes/filterRoutes.js
+++ src/routes/filterRoutes.js
@@ -15,3 +15,47 @@ router.get('/api/filters', async (req, res) => {
 });
 
 export default router;
+
+
+
+/**
+ * @swagger
+ * tags:
+ *   name: Filters
+ *   description: Отримання категорій та цінових діапазонів для фільтрації товарів
+ */
+
+/**
+ * @swagger
+ * /filters:
+ *   get:
+ *     tags: [Filters]
+ *     description: Отримання категорій та цінових діапазонів для фільтрації товарів
+ *     security: []
+ *     summary: Отримати параметри фільтрів товарів (категорії, мін/макс ціна)
+ *     responses:
+ *       200:
+ *         description: Фільтри успішно отримано
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 categories:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       _id:
+ *                         type: string
+ *                       name:
+ *                         type: string
+ *                 minPrice:
+ *                   type: number
+ *                   example: 100
+ *                 maxPrice:
+ *                   type: number
+ *                   example: 1000
+ *       500:
+ *         description: Внутрішня помилка сервера
+ */
